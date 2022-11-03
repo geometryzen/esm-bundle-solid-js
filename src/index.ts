1521ca1e446f0dbdf5accaea6630761c904b010c
David Geo Holmes <geometryzen@gmail.com>
2022-11-03 18:48:52 -0400

    Bundle sub-modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import html from 'solid-js/html';
-export * from 'solid-js';
-export { render } from 'solid-js/web';
+export { $DEVCOMP, $PROXY, $TRACK, Accessor, batch, children, ChildrenReturn, Context, createComputed, createContext, createDeferred, createEffect, createMemo, createReaction, createRenderEffect, createResource, createRoot, createSelector, createSignal, DEV, enableExternalSource, enableScheduling, equalFn, getListener, getOwner, InitializedResource, InitializedResourceOptions, InitializedResourceReturn, JSX, JSXElement, on, onCleanup, onError, onMount, Owner, Resource, ResourceActions, ResourceFetcher, ResourceFetcherInfo, ResourceOptions, ResourceReturn, ResourceSource, ReturnTypes, runWithOwner, Setter, Signal, startTransition, untrack, useContext, useTransition } from 'solid-js';
+export { $RAW, ArrayFilterFn, createMutable, createStore, modifyMutable, NotWrappable, Part, produce, reconcile, ReconcileOptions, SetStoreFunction, SolidStore, Store, StoreNode, StorePathRange, StoreSetter, unwrap } from 'solid-js/store';
+export { createRenderer } from 'solid-js/universal';
+export { addEventListener, Aliases, Assets, assign, ChildProperties, classList, className, clearDelegatedEvents, createComponent, DelegatedEvents, delegateEvents, DOMElements, Dynamic, dynamicProperty, effect, ErrorBoundary, escape, For, generateHydrationScript, getAssets, getHydrationKey, getNextElement, getNextMarker, getNextMatch, hydrate, Hydration, HydrationScript, Index, innerHTML, insert, isServer, LegacyResults, Match, memo, mergeProps, MountableElement, NoHydration, Portal, PropAliases, Properties, render, renderToStream, renderToString, renderToStringAsync, resolveSSRNode, setAttribute, setAttributeNS, Show, spread, ssr, ssrAttribute, ssrClassList, ssrElement, ssrHydrationKey, ssrStyle, style, Suspense, SuspenseList, SVGElements, SVGNamespace, Switch, template, useAssets } from 'solid-js/web';
 export { html };
+
