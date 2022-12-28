2487c2f2f7bfd670a828da650ea2b196eeedabfe
David Geo Holmes <geometryzen@gmail.com>
2022-12-28 15:19:19 -0500

    sub-modules

--- esm/es5/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * @geometryzen/solid-js@0.9.12 is a "module" format for solid-js@1.6.6
- * © 2022 David Geo Holmes
- * Released under the MIT License.
- */
-export { $ as $DEVCOMP, a as $PROXY, b as $TRACK, D as DEV, E as ErrorBoundary, F as For, I as Index, M as Match, S as Show, c as Suspense, d as SuspenseList, e as Switch, f as batch, g as cancelCallback, h as children, i as createComponent, j as createComputed, k as createContext, l as createDeferred, m as createEffect, n as createMemo, o as createReaction, p as createRenderEffect, q as createResource, r as createRoot, s as createSelector, t as createSignal, u as createUniqueId, v as enableExternalSource, w as enableHydration, x as enableScheduling, y as equalFn, z as from, A as getListener, B as getOwner, C as indexArray, G as lazy, H as mapArray, J as mergeProps, K as observable, L as on, N as onCleanup, O as onError, P as onMount, Q as requestCallback, R as resetErrorBoundaries, T as runWithOwner, U as sharedConfig, V as splitProps, W as startTransition, X as untrack, Y as useContext, Z as useTransition } from './shared.js';
